39be513bee3475bc78d7810b1da4e5168366240d
Matthew Emes <memes@matthewemes.com>
2023-03-16 14:37:11 -0700

    feat: Remove obsolete region-location submodule
    This functionality has been published in my region-detail module.

--- modules/region-locations/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lookup" {
-  value       = local.lookup
-  description = <<EOD
-A map of GCP compute region to a coordinate object with latitude and longitude
-fields.
-EOD
-}
--- modules/region-locations/variables.tf
+++
@@ -1,1 +0,0 @@
-# Module contains no input variables
