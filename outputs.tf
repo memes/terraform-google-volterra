ee8626832fc3f8a8cd7c6792b6f27c2a1d0b1799
Matthew Emes <memes@matthewemes.com>
2025-03-18 11:46:40 -0700

    fix: Add private/public SLO and SLI addresses

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,12 @@ output "smsv2_site_id" {
 
 output "nodes" {
   value = { for k, v in google_compute_instance.node : k => {
-    self_link = v.self_link
-    zone      = v.zone
+    self_link     = v.self_link
+    zone          = v.zone
+    slo_ip        = v.network_interface[0].network_ip
+    slo_public_ip = try(v.network_interface[0].access_config.nat_ip, null)
+    sli_ip        = try(v.network_interface[1].network_ip, null)
+    sli_public_ip = try(v.network_interface[1].access_config.nat_ip, null)
   } }
   description = <<-EOD
     A map of CE node names to values
